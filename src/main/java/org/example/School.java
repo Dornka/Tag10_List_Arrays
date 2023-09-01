89d937b55f5f8442385a94e41f1aca09b2299617
Dornka <Skiber@gmail.com>
2023-09-01 11:37:27 +0200

    added class school and student added methods to print

--- src/main/java/org/example/School.java
+++ src/main/java/org/example/School.java
@@ -1,2 +1,24 @@
-package org.example;public class School {
+package org.example;
+import java.util.List;
+import java.util.ArrayList;
+
+public class School {
+
+    private final List<Student> studentList;
+
+    public School() {
+        this.studentList = new ArrayList<>();
+
+    }
+
+    public void addStudent (Student student) {
+        studentList.add(student);
+
+    }
+
+    public void printAllStudents() {
+        for (Student student : studentList) {
+            System.out.println(student);
+        }
+    }
 }
--- src/main/java/org/example/Student.java
+++ src/main/java/org/example/Student.java
@@ -1,2 +1,42 @@
-package org.example;public class Student {
+package org.example;
+
+public class Student {
+    private String firstname;
+    private String lastname;
+    private int matriculationnumber;
+
+    public Student (String firstname, String lastname, int matriculationnumber){
+        this.firstname = firstname;
+        this.lastname = lastname;
+        this.matriculationnumber = matriculationnumber;
+    }
+
+    public String getFirstname() {
+        return firstname;
+    }
+
+    public void setFirstname(String firstname){
+        this.firstname = firstname;
+    }
+    public String getLastname() {
+        return lastname;
+    }
+
+    public void setLastname(String lastname){
+        this.lastname = lastname;
+    }
+    public int getMatriculationnumber() {
+        return matriculationnumber;
+    }
+
+    public void setMatriculationnumber(int matriculationnumber){
+        this.matriculationnumber = matriculationnumber;
+    }
+    public String toString() {
+
+        return "Student:" +
+                " Vorname='" + firstname + '\'' +
+                " nachname='" + lastname + '\'' +
+                " matrikelnummer='" + matriculationnumber + "'";
+    }
 }
